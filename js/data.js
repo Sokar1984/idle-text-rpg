0e2bcab5b9c4121215342f5c0fee0545e7aa7ad2
Sokar1984 <131913443+Sokar1984@users.noreply.github.com>
2026-08-19 23:07:21 +0200

    Load races data

--- js/data.js
+++ js/data.js
@@ -1,7 +1,8 @@
 export async function loadData() {
-  const [config, classes, locations, monsters, events] = await Promise.all([
+  const [config, classes, races, locations, monsters, events] = await Promise.all([
     fetch('data/config.json').then(r => r.json()),
     fetch('data/classes.json').then(r => r.json()),
+    fetch('data/races.json').then(r => r.json()),
     fetch('data/locations.json').then(r => r.json()),
     fetch('data/monsters.json').then(r => r.json()),
     fetch('data/events.json').then(r => r.json())
@@ -10,6 +11,7 @@ export async function loadData() {
   return {
     config,
     classes: classes.classes,
+    races: races.races,
     locations: locations.locations,
     monsters: monsters.monsters,
     events: events.events
