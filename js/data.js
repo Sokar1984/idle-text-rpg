630deedd85bb2ecd24a5f056355a655c41c9bacc
Sokar1984 <131913443+Sokar1984@users.noreply.github.com>
2026-08-19 23:34:07 +0200

    Load items data for economy

--- js/data.js
+++ js/data.js
@@ -1,11 +1,12 @@
 export async function loadData() {
-  const [config, classes, races, locations, monsters, events] = await Promise.all([
+  const [config, classes, races, locations, monsters, events, items] = await Promise.all([
     fetch('data/config.json').then(r => r.json()),
     fetch('data/classes.json').then(r => r.json()),
     fetch('data/races.json').then(r => r.json()),
     fetch('data/locations.json').then(r => r.json()),
     fetch('data/monsters.json').then(r => r.json()),
-    fetch('data/events.json').then(r => r.json())
+    fetch('data/events.json').then(r => r.json()),
+    fetch('data/items.json').then(r => r.json())
   ]);
 
   return {
@@ -14,6 +15,7 @@ export async function loadData() {
     races: races.races,
     locations: locations.locations,
     monsters: monsters.monsters,
-    events: events.events
+    events: events.events,
+    items
   };
 }
